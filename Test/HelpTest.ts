ec1726add25edd2c67c2cc6a5afbb37d45fd7c27
Gleisson Neves <53718407+gdk46@users.noreply.github.com>
2021-03-11 04:12:39 -0300

    recursos para teste unitários

---
+++ Test/HelpTest.ts
@@ -0,0 +1,44 @@
+//! occasionTS.ts
+//! version : 2.29.1
+//! authors : Gleisson Neves, OccasionTS.ts contributors
+//! license : MIT
+//! url
+
+import Help from '../Support/help';
+
+export default class HelpTest
+{
+    help:object;
+
+    constructor() {
+        this.help = new Help();
+    }
+
+    /**
+     * 
+     */
+    dataTypeIsEmpty() {
+        
+    }
+
+    /**
+     * 
+     */
+     dataTypeIsUndefined() {
+
+    }
+
+    /**
+     * 
+     */
+     dataTypeIsNumber() {
+
+    }
+
+    /**
+     * 
+     */
+     dataTypeIsArray() {
+
+    }
+}
